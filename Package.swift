d2b3b34d0b185ca2151b441ad6c927b884e92c9e
antonmes <antonmes@users.noreply.github.com>
2016-03-16 08:22:16 +0300

    Remove C deps

--- Package.swift
+++ Package.swift
@@ -3,10 +3,7 @@ import PackageDescription
 let package = Package(
     name: "HTTPClient",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/TCP.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 3),
-        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/Zewo/TCP.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 4),
     ]
 )
