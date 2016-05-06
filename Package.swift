fbee6931029a797523755ebd2c5db0c4713497c4
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-05-06 15:02:13 +0900

    Updated for 05-03
    updated dependencies
    temporary using under tomohisa/ till zewo/ PR passed

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "HTTPClient",
     dependencies: [
         .Package(url: "https://github.com/tomohisa/TCP.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/Zewo/HTTPSerializer.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/tomohisa/HTTPParser.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/tomohisa/HTTPSerializer.git", majorVersion: 0, minor: 7),
     ]
 )
