b5309bebe0ac63fd24f20b2b74d1352081edc0f6
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-05-06 17:57:23 +0900

    Updated Package

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "HTTPClient",
     dependencies: [
-        .Package(url: "https://github.com/tomohisa/TCP.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/tomohisa/HTTPParser.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/tomohisa/HTTPSerializer.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/VeniceX/TCP.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/HTTPSerializer.git", majorVersion: 0, minor: 7),
     ]
 )
--- README.md
+++ README.md
@@ -23,7 +23,7 @@ import PackageDescription
 
 let package = Package(
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/HTTPClient.git", majorVersion: 0, minor: 5)
+        .Package(url: "https://github.com/VeniceX/HTTPClient.git", majorVersion: 0, minor: 6)
     ]
 )
 ```
