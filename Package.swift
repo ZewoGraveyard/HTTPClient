103b24e2ef5cf7a9ac8985e7a35c311a30e9027e
Tomohisa Takaoka <tomohisa.takaoka@gmail.com>
2016-05-06 17:59:12 +0900

    Update for 2016-05-03 (#8)
    * Update for 05-03 Snapshot
    
    * Updated for 05-03
    
    updated dependencies
    temporary using under tomohisa/ till zewo/ PR passed
    
    * Updated Package

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "HTTPClient",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/TCP.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/Zewo/HTTPSerializer.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/VeniceX/TCP.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/HTTPSerializer.git", majorVersion: 0, minor: 7),
     ]
 )
